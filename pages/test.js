f41a5594279eb7dc888accacbbd14ad76f5575c1
Sareena <sareena.naser@ibm.com>
2022-02-23 11:04:26 +0000

    test pages

--- pages/test.js
+++ pages/test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import css from '../styles/Landing.module.css'
+import css from '../styles/Test.module.css'
 
 const Test = () => {
     return (
