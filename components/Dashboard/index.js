66f6e4afaf636c4471e30e66850c7b97f7e3f81f
AmandaRichards <Richards.amanda92@gmail.com>
2022-02-23 12:29:44 +0000

    row attempt

--- components/Dashboard/index.js
+++ components/Dashboard/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
 import DashboardItem from '../DashboardItem';
 import css from './Dashboard.module.css';
 import Link from 'next/link';
+import DashboardChart from '../DashboardChart';
+
 
 
 
@@ -14,12 +17,22 @@ const Dashboard = () => {
         
             
         <div className={css.dashBox}>
-            <p className={css.dashHeader}>User dashboard: analyse and monitor your food use:</p>
-            <div>
-            <DashboardItem className={css.dashRight} onClick={handleClick} text={`Food donated`}/>
-            <DashboardItem className={css.dashCentre} onClick={handleClick} text={`Wastage meter`}/>
-            <DashboardItem className={css.dashLeft} onClick={handleClick} text={`Consumption`}/>
-        </div>
+        <Row> <p className={css.dashHeader}>User dashboard: analyse and monitor your food use:</p></Row>
+        <Row> 
+        <Col xs={4}><DashboardChart className={css.dashRight} onClick={handleClick} text={`Food donated`}/></Col>
+        <Col xs={4}><DashboardChart className={css.dashCentre} onClick={handleClick} text={`Wastage meter`}/></Col>
+        <Col xs={4}><DashboardChart className={css.dashLeft} onClick={handleClick} text={`Consumption`}/></Col>  
+        </Row>
+
+        <Row> 
+        <Col xs={4}>Wastage</Col>
+        <Col xs={4}>Donations</Col>
+        <Col xs={4}>Consumption</Col>
+        </Row>
+        
+           
+            
+          
   
         </div>
     )
--- pages/landing.js
+++ pages/landing.js
@@ -8,7 +8,7 @@ const Landing = () => {
         <div>
             <NavbarBtn />
             <Dashboard />
-            <DashboardChart />
+            
         </div>
     );
 };
