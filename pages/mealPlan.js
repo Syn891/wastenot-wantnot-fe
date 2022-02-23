e29de8240966eef263f6fb7a821e1879b8a5c6f3
Sareena <sareena.naser@ibm.com>
2022-02-23 11:01:51 +0000

    bootstrap runthrough

--- pages/mealPlan.js
+++ pages/mealPlan.js
@@ -1,16 +1,28 @@
 import RecipieInfoDisplay from "../components/RecipieInfoDisplay";
 import Navbar from "../components/Navbar";
 import SubPageNavBtn from "../components/SubPageNavBtn";
+import {Row} from 'react-bootstrap'
 
 const MealPlan = () => {
   return (
     <div>
-      <h1>hello im the meal plan</h1>
-      <Navbar title={"Meal Planner"} />
+      <Row>
+        <h1>hello im the meal plan</h1>
+      </Row>
+
+      <Row>
+        <Navbar title={"Meal Planner"} />
+      </Row>
+
+      <Row>
       <div>
         <SubPageNavBtn buttonName={"Recipes"} />
         <SubPageNavBtn buttonName={"My Meals"} />
       </div>
+      </Row>
+
+      <Row>
+        
       <RecipieInfoDisplay
         image={
           "https://ih1.redbubble.net/image.2929924990.5213/raf,128x128,075,f,grey_lightweight_hoodie.jpg"
@@ -44,6 +56,7 @@ const MealPlan = () => {
           "https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwi0y7ruz5P2AhWXFMAKHZQkBbUQPAgI"
         }
       />
+      </Row>
     </div>
   );
 };
