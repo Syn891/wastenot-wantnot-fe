09edc2282b2febb6a120246769b1f5cb0eedd0e8
gabrielsterponemagni <94461239+gabrielsterponemagni@users.noreply.github.com>
2022-02-24 16:27:08 +0000

    more styling

--- components/Banner/index.js
+++ components/Banner/index.js
@@ -6,7 +6,7 @@ import css from './Banner.module.css'
 const Banner = ({title}) => {
     return (
       <div>
-    <Col className={css.header}><h1>{title}</h1></Col>
+    <Col className={css.header}><h1 className={css.h1}>{title}</h1></Col>
     <Col className={css.branding}><img src="https://i.ibb.co/MV46RZx/Recycle-icon-by-rudezstudio-2-580x386-copy.png"></img>
     </Col>
     </div>
