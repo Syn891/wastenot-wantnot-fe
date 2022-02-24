1915be29ae5c6521db41971d21429ce74cc3fd48
tomdichmont <t.dichmont@gmail.com>
2022-02-24 14:44:51 +0000

    Add checked Item to Pantry button, and, Create new list button, first drafts

--- pages/shoppinglist.js
+++ pages/shoppinglist.js
@@ -6,6 +6,9 @@ import { Col, Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import css from "../styles/Shoppinglist.module.css";
 import AddItemButton from "../components/AddItemButton";
+import AddItemToPantryButton from "../components/AddItemToPantryButton";
+import CreateNewListButton from "../components/CreateNewListButton";
+
 // $ database contains _id, id(string), shopping_items(array)[{_itemid(again), name, est_exp, category, quantity, measurement,_id(same as root_id)}],user_id
 
 function ShoppingList() {
@@ -20,13 +23,13 @@ function ShoppingList() {
               <SwipeBar />
             </Row>
             <Row className={css.row}>
-              <AddItemButton message={"Add Item to Grocery List"}/>
+              <AddItemButton message={"Add item to List"}/>
             </Row>
             <Row className={css.row}>
-              <Button>Add to pantry button</Button>
+              <AddItemToPantryButton message={"Add checked list items to My Pantry:"}/>
             </Row>
             <Row className={css.row}>
-              <Button>Create New List</Button>
+              <CreateNewListButton message={"Create a new List"}/>
             </Row>
             <DonationPromptInfo />
           </Col>
