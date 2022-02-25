2d8c0235dbbbd266b05d5bb1185ae08e09a860bc
AmandaRichards <Richards.amanda92@gmail.com>
2022-02-25 09:31:19 +0000

    dashboard charts work in apiTest page

--- pages/apiTest.js
+++ pages/apiTest.js
@@ -5,14 +5,15 @@ import Dashboard from '../components/Dashboard';
 const Test = () => {
 
     const [waste, setWaste] =useState(0);
-    // const [donations, setDonations] = useState();
-    // const [consumption, setConsumption]= useState();
+    const [donations, setDonations] = useState(0);
+    const [consumption, setConsumption]= useState(0);
 
  useEffect(()=>{
     async function getUserWastage(){
-        const id="620fad130ff39d66394b8b64"
+        const id="auth0|6217832025ea850070393ba0"
         const response = await fetch(`https://waste-want.herokuapp.com/users/${id}`)
         const data= await response.json();
+        console.log(data.payload)
         setWaste(data.payload.wastage)
 
 
@@ -20,38 +21,37 @@ const Test = () => {
     getUserWastage();
 }, [waste])
 
-// const [donations, setDonations] = useState();
-
-//  useEffect(()=>{
-//     async function getUserDonations(){
-//         const id="620fad130ff39d66394b8b64"
-//         const response = await fetch(`https://waste-want.herokuapp.com/users/${id}`)
-//         const data= await response.json();
-//         setDonations(data.payload.donations)
 
 
-//     }
-//     getUserDonations();
-// }, [donations])
-
-//     const [consumption, setConsumption]= useState();
-
-// useEffect(()=>{
-//     async function getUserConsumption(){
-//         const id="620fad130ff39d66394b8b64"
-//         const response = await fetch(`https://waste-want.herokuapp.com/users/${id}`)
-//         const data= await response.json();
-//         setConsumption(data.payload.consumption)
+ useEffect(()=>{
+    async function getUserDonations(){
+        const id="auth0|6217832025ea850070393ba0"
+        const response = await fetch(`https://waste-want.herokuapp.com/users/${id}`)
+        const data= await response.json();
+        setDonations(data.payload.donations)
 
 
-//     }
-//     getUserConsumption();
-// }, [consumption])
+    }
+    getUserDonations();
+}, [donations])
+
+
+useEffect(()=>{
+    async function getUserConsumption(){
+        const id="auth0|6217832025ea850070393ba0"
+        const response = await fetch(`https://waste-want.herokuapp.com/users/${id}`)
+        const data= await response.json();
+        setConsumption(data.payload.consumption)
+
+
+    }
+    getUserConsumption();
+}, [consumption])
 
   return (
     <div>
     <p>waste:{waste}</p>
-    <Dashboard waste={waste}  link="/userInformation"/>
+    <Dashboard waste={waste} donations={donations} consumption={consumption} link="/userInformation"/>
     </div>
   )
 }
@@ -61,4 +61,4 @@ const Test = () => {
 
 export default Test;
 
-// donations={donations} consumption={consumption}
+// 
