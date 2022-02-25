e5474bad60a2214807d54734137e0b108e3a8cee
Sareena <sareena.naser@ibm.com>
2022-02-25 17:03:00 +0000

    swipe function on pantry list items

--- components/FoodListItem/index.js
+++ components/FoodListItem/index.js
@@ -4,19 +4,19 @@ import css from './FoodListItem.module.css'
 
 function FoodListItem({ name, est_exp, category, quantity, measurement, color }) {
   console.log(name, est_exp, category, quantity, measurement);
-  const date = Math.trunc(Number(est_exp.$date.$numberLong) / 10000000);
+  // const date = Math.trunc(Number(est_exp.$date.$numberLong) / 10000000);
   
     if (!color) {
      color = "#5CC971"
   }
   //this is while we work out the date solution for the DB just to make the number shorter
   return (
-    < >
+    <>
       <Col  className={css.col} xs={{span:3}}>
         <div>{name}</div>
       </Col>
       <Col  className={css.col} xs={{span:3}}>
-        <div>{date}</div>
+        <div>1/1/11</div>
         {/* {est_exp.$date.$numberLong} */}
       </Col>
       <Col  className={css.col} xs={{span:2}}>
--- pages/pantry.js
+++ pages/pantry.js
@@ -8,9 +8,23 @@ import { Container } from 'react-bootstrap';
 import FoodCategoryRow from '../components/FoodCategoryRow';
 import FoodListItem from '../components/FoodListItem'
 import SwipePantryBar from '../components/SwipePantryBar';
-const Pantry = () => {
-    const router = useRouter()
+import { useUser, getSession } from '@auth0/nextjs-auth0';
+import SwipeBar from '../components/SwipeBar';
 
+
+const Pantry = ({pantryItems}) => {
+    
+    let user = useUser();
+    let filteredItems = []
+    
+    if(pantryItems.payload.length > 0){
+        pantryItems.payload.map((p)=> {
+        if(p.user_id === user.user.sub) {
+            filteredItems.push(p)   
+        }
+    })}
+
+    const router = useRouter()
     const setColor = (number) => {
         let color = ''
         if (number < 3){
@@ -34,11 +48,28 @@ const Pantry = () => {
          <FoodCategoryRow />
 
         <Container className={css.container}>
-        <SwipePantryBar />
+        {filteredItems.map((f)=> {
+            return f.pantry_items.map((pi)=> {
+                return <SwipePantryBar>            
+                    {/* <FoodListItem color={setColor(1)} name={pi?.name ? pi.name : ""} quantity={pi?.quantity ? pi.quanity : ""} measurement={pi?.measurement ? pi.measurement : ""} /> */}
+                    <FoodListItem color={setColor(1)} name={pi.name} quantity={ pi.quanity} measurement={pi.measurement} />
 
+                </SwipePantryBar>
+            })
+        })}
         </Container>
+
+        <SwipeBar />
         </div>
     );
 };
 
+
+export async function getServerSideProps(user) {
+        let pantryItems = await fetch(`http://localhost:3001/pantrylist/`)
+        pantryItems = await pantryItems.json()
+    
+        return {props: {pantryItems: pantryItems}}
+    
+}
 export default Pantry;
