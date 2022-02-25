2da7bdf121ad42d0dd42e1ad0f12c17c6723b254
Sareena <sareena.naser@ibm.com>
2022-02-25 11:26:13 +0000

    pantry page created

--- pages/landing.js
+++ pages/landing.js
@@ -69,7 +69,7 @@ const Landing = () => {
                     <NavigationButton title="Grocery List" color="#94DEB2" link="/shoppinglist"Icon={TiShoppingCart}/>
                 </Col>
                 <Col className={css.col}>
-                    <NavigationButton title="In my Pantry" color="#5CC971" link="/"Icon={RiFridgeLine}/>
+                    <NavigationButton title="In my Pantry" color="#5CC971" link="/pantry"Icon={RiFridgeLine}/>
                 </Col>
                 <Col className={css.col}>
                     <NavigationButton title="Meal Planner" color="#F1AC79" link="/mealPlan"Icon={GiForkKnifeSpoon}/>
---
+++ pages/pantry.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pantry = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Pantry;
