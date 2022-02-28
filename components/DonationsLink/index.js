ce13b168d2451be6c24e35d5eeaf3f8c4026698c
AmandaRichards <Richards.amanda92@gmail.com>
2022-02-28 09:48:49 +0000

    friday

---
+++ components/DonationsLink/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import css from './DonationsLink.module.css';
+import { Row, Col } from 'react-bootstrap';
+import {GrMapLocation} from 'react-icons/Gr'; 
+import Link from 'next/link'
+
+const DonationsLink = ({link}) => {
+    return (
+        <Link href={link}>
+        <Row className={css.box}>
+        <Col sm={4} className={css.col}>
+        <p className={css.p}>What people need near me?
+Donations needed in your area:</p>
+</Col>
+<Col sm={8} classNme={css.col} >
+<GrMapLocation />
+
+</Col>
+            
+        </Row>
+        </Link>
+    )
+}
+
+export default DonationsLink
