a55254129c17f9fd7794ccf93f7672a8258b4af7
Sareena <sareena.naser@ibm.com>
2022-03-07 11:56:57 +0000

    add to consumption

--- pages/pantry.js
+++ pages/pantry.js
@@ -43,45 +43,23 @@ const Pantry = () => {
         return color;
       };
 
-    function isCheckedFunc(position) {
-    
-            let tempState  = [...isChecked]
-            tempState[position] = !tempState[position]
-            setIsChecked(tempState)
-        
-            let donationsfaves = await useFetch('donationbank', 'GET', null, `/?user_id=${user.user.sub}`)
-            console.log(donationsfaves)
+    async function isCheckedFunc(id) {
             
-            if(donationsfaves.payload.length < 1 && tempState[position] === true){
-        
-              let object = {
-                user_id: user.user.sub,
-                donation_banks: [{
-                  ...foodBanks[position]
-                }]
-              }
-                await useFetch('donationbank', 'POST', object, '' )
-             } else if (tempState[position] === true) {
-        
-              var containing = donationsfaves.payload[0].donation_banks.find(function(ele) {
-                return ele.name === foodBanks[position].name;
-              });
-              
-               if(containing === undefined) {
-                      
-                let query = {donation_banks: foodBanks[position]}
-                useFetch('donationbank', 'PUT', query,`/update/?user_id=${user.user.sub}` )
-               }
-        
-            } else if (tempState[position] === false ){
-              var containing = donationsfaves.payload[0].donation_banks.find(function(ele) {
-                return ele.name === foodBanks[position].name;
-              });
-                      const remove = {donation_banks: {_id:containing._id}}
-                let data = useFetch('donationbank', 'DELETE', remove, `/?user_id=${user.user.sub}`)
-                data = await Promise.resolve(data)
+                const remove = {pantry_items: {_id:id}}
+                const res = useFetch('pantryList', 'DELETE', remove, `/?user_id=${user.user.sub}`)
+                await Promise.resolve(res)    
+
+                let userInfo = useFetch('users', 'GET', null, `/${user.user.sub}`)
+                userInfo = await Promise.resolve(userInfo)
+                let data = userInfo.payload.consumption + 1
                 console.log(data)
-            }
+            
+                  let query = {consumption: data}
+                  let res1 = useFetch('users', 'PUT', query,`/${user.user.sub}` )
+                  res1 = await Promise.resolve(res1)
+                  console.log(res1)
+                console.log(isChecked)
+                userPantry()
      
     }
 
@@ -148,7 +126,7 @@ const Pantry = () => {
                 object_id={f._id}
                 object={object}>
                     
-                    <PantryListItem onChange={() => isCheckedFunc(index)} color={setColor(dateDif)} name={pi?.name ? pi.name : ""} quantity={pi?.quantity ? pi.quantity : ""} measurement={pi?.measurement ? pi.measurement : ""} expiry={dateString}/>
+                    <PantryListItem onChange={() => isCheckedFunc(pi._id, index)} color={setColor(dateDif)} name={pi?.name ? pi.name : ""} quantity={pi?.quantity ? pi.quantity : ""} measurement={pi?.measurement ? pi.measurement : ""} expiry={dateString}/>
                 </SwipePantryBar>
             })
         })
