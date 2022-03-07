0d7f8e891a9e2366d9d0eb4606172db065b1a928
Sareena <sareena.naser@ibm.com>
2022-03-07 14:19:17 +0000

    fixed icon import

--- components/DonationsLink/index.js
+++ components/DonationsLink/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import css from './DonationsLink.module.css';
 import { Row, Col } from 'react-bootstrap';
-import {GrMapLocation} from 'react-icons/Gr'; 
+import {GrMapLocation} from 'react-icons/gr'; 
 import Link from 'next/link'
 
 const DonationsLink = ({link}) => {
