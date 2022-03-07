96c4b069b8f17a77dc6f53c584b6e772ef37ee46
Sareena <sareena.naser@ibm.com>
2022-03-07 13:37:34 +0000

    mc

--- pages/pantry.js
+++ pages/pantry.js
@@ -46,46 +46,6 @@ const Pantry = () => {
       };
 
     async function isCheckedFunc(id, position) {
-    
-<<<<<<< HEAD
-            // let tempState  = [...isChecked]
-            // tempState[position] = !tempState[position]
-            // setIsChecked(tempState)
-        
-            // let donationsfaves = await useFetch('donationbank', 'GET', null, `/?user_id=${user.user.sub}`)
-            // console.log(donationsfaves)
-            
-            // if(donationsfaves.payload.length < 1 && tempState[position] === true){
-        
-            //   let object = {
-            //     user_id: user.user.sub,
-            //     donation_banks: [{
-            //       ...foodBanks[position]
-            //     }]
-            //   }
-            //     await useFetch('donationbank', 'POST', object, '' )
-            //  } else if (tempState[position] === true) {
-        
-            //   var containing = donationsfaves.payload[0].donation_banks.find(function(ele) {
-            //     return ele.name === foodBanks[position].name;
-            //   });
-              
-            //    if(containing === undefined) {
-                      
-            //     let query = {donation_banks: foodBanks[position]}
-            //     useFetch('donationbank', 'PUT', query,`/update/?user_id=${user.user.sub}` )
-            //    }
-        
-            // } else if (tempState[position] === false ){
-            //   var containing = donationsfaves.payload[0].donation_banks.find(function(ele) {
-            //     return ele.name === foodBanks[position].name;
-            //   });
-            //           const remove = {donation_banks: {_id:containing._id}}
-            //     let data = useFetch('donationbank', 'DELETE', remove, `/?user_id=${user.user.sub}`)
-            //     data = await Promise.resolve(data)
-            //     console.log(data)
-            // }
-=======
             let tempState  = [...isChecked]
             tempState[position] = !tempState[position]
             setIsChecked(tempState)
@@ -104,7 +64,6 @@ const Pantry = () => {
                   res1 = await Promise.resolve(res1)
                   console.log(res1)
                 console.log(isChecked)
->>>>>>> 1c07767c345ae114d2b31661b0240a3611dc426a
      
     }
 
