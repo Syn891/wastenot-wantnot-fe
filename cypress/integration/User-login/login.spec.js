1503c3fe121fb8d07a26b1deb728914c08b3a703
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2022-03-10 16:16:46 +0000

    login-page

---
+++ cypress/integration/User-login/login.spec.js
@@ -0,0 +1,55 @@
+/// <reference types="cypress" />
+
+// Welcome to Cypress!
+//
+// This spec file contains a variety of sample tests
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
+
+describe('Page loading', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('https://wastenot-wantnot.netlify.app/')
+  })
+
+  it('display login button', () => {
+    // We use the `cy.get()` command to get all elements that match the selector.
+    // Then, we use `should` to assert that there are two matched items,
+    // which are the two default items.
+    cy.get('.btn-primary').should('have.text', 'Login / Signup')
+
+    // We can go even further and check that the default todos each contain
+    // the correct text. We use the `first` and `last` functions
+    // to get just the first and last matched elements individually,
+    // and then perform an assertion with `should`.
+    // cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
+    // cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
+  })
+  
+  it('display banner and logo on the centre of the page', () => {
+
+    cy.get('.Home_headingT__LUled').should('have.text', 'WasteNot:WantNot!')
+    cy.get('img').filter('[src]').filter(':visible')
+  })
+
+  it('Homepage video playing', () => {
+    cy.get('.Home_video__wXJek').should('have.prop', 'paused', false).and('have.prop', 'ended', false)
+  })
+
+  it('display logo on top right corner of the page', () => {
+    cy.get('img').filter('[src]').filter(':visible')
+  })
+
+  it('Burger bar being able to click', () => {
+  cy.get('.navbar-toggler').should('be.visible').wait(1000).click()
+  cy.get(".Home_offcanvasHeader__pP80n").should('have.text', 'WasteNot: WantNot').should('be.visible').wait(1000).click().filter('[src]').filter(':visible')
+})
+})
